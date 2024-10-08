04135e4855b2f9275ed394bab2317474deba3650
rcedi1012 <rocedi@utexas.edu>
2024-10-08 07:42:29 -0500

    ye

--- classes.js
+++ classes.js
@@ -1,15 +1,18 @@
 class reptile {
     constructor() {
-        this.x = windowWidth * 0.71;
+        this.x = windowWidth * 0.15;
         this.y = aspectH * 0.5;
         this.size = windowWidth * 0.025;
         this.cary = aspectH;
+        this.color = color(55);
     }
     
       show() {
         stroke(0);
-        fill(55);
+        fill(this.color);
         ellipse(this.x, this.y, this.size *2);
+        fill(255);
+        text('reptile',this.x - windowWidth * 0.025,this.y);
       }
     
       hover() {
@@ -17,22 +20,22 @@ class reptile {
           caranim1 = true;
           text(caranim1, windowWidth * 0.5, aspectH * 0.5);
         }
-        else {
-          caranim1 = false;
-        }
       }
 
       car() {
         if ((caranim1 == true) && (this.cary > -50)) {
             rect(this.x - (windowWidth * 0.025), this.cary, windowWidth * 0.05, aspectH * 0.075);
             this.cary -= aspectH * 0.075;
+            if (this.cary < aspectH * 0.5) {
+                this.color = color(227, 24, 10);
+            }
         }
       }
 }
 
 class mammal {
     constructor() {
-        this.x = windowWidth * 0.15;
+        this.x = windowWidth * 0.25;
         this.y = aspectH * 0.5;
         this.size = windowWidth * 0.025;
     }
@@ -43,3 +46,9 @@ class mammal {
         ellipse(this.x, this.y, this.size *2);
     }
 }
+
+//things to do:
+//create art
+//animal hit/animal breaked/cars initiated score
+//the actual graph part
+//if user breaks for all the animals, good ending
